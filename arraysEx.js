f73c8d2b34eb3fb1323a2cf638f6bcc626fc3e44
BurcuHaddad <burcuhaddad@gmail.com>
2023-01-12 06:58:04 +0300

    Array methods

---
+++ arraysEx.js
@@ -0,0 +1,22 @@
+console.log("Original Array");
+let myArray=[1,2,3,4];
+console.log(myArray)
+console.log("Adding element to the end");
+myArray.push(5,6,"a");
+console.log(myArray)
+console.log("Adding element to the start");
+myArray.unshift("x", 0)
+console.log(myArray)
+console.log("Removing element from the end");
+myArray.pop()
+console.log(myArray)
+console.log("Removing element from the start");
+myArray.shift()
+console.log(myArray)
+
+
+
+
+
+
+
