3da4c9c74097a015a61cea5aa7838fbd60aeda1b
BurcuHaddad <burcuhaddad@gmail.com>
2023-01-12 16:57:50 +0300

    functions

---
+++ accessObjectProperties.js
@@ -0,0 +1,8 @@
+const person={
+    firstName:"John",
+    lastName:"Doe",
+    id: 5566
+};
+
+console.log(person.firstName + " "+ person.lastName)
+console.log(person["firstName"]+ " "+person["lastName"])
---
+++ functionVariable.js
@@ -0,0 +1,4 @@
+function toCelcius(fahrenheit) {
+    return (5/9)*(fahrenheit-32);
+}
+console.log("The temperature is " + toCelcius(77)+ " Celcius")
