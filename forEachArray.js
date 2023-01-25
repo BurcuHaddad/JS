f03902b9f3549ea92a8d0b775c745c63e036f96e
BurcuHaddad <burcuhaddad@gmail.com>
2023-01-25 14:54:12 +0300

    loops

---
+++ forEachArray.js
@@ -0,0 +1,8 @@
+const numbers = [45, 4, 9, 16, 25];
+
+
+numbers.forEach(myFunc);
+
+function myFunc(value, index, array) {
+    console.log(value);
+}
---
+++ forOfArray.js
@@ -0,0 +1,6 @@
+const cars = ["BMW", "Volvo", "Mini"];
+
+let text = "";
+for (let c of cars) {
+    console.log(c + "\n");
+}
